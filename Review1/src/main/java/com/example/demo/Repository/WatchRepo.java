a4196cab582675f5446bc8d8c16cd7f3b74fbce5
AjmalAha <ajmalahamedr@gmail.com>
2023-04-25 10:30:05 +0530

    review1

--- Review1/src/main/java/com/example/demo/Repository/WatchRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.WatchModel;
-
-public interface WatchRepo extends JpaRepository<WatchModel, Integer> {
-
-}1
